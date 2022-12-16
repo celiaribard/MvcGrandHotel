370789b4ac878aab2d32fee6c11ea2f5fd2fbc44
Célia Ribard Del Castillo <celia.rdc31@gmail.com>
2022-12-16 13:51:32 +0100

    initialisation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcGrandHotel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
